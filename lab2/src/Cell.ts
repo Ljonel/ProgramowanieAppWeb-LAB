901398b6ee592010157301232fbe5f059fc85d99
Damian Cygan <damian.cygan@microsoft.wsei.edu.pl>
2021-03-25 11:28:00 +0100

    tic-tac-toe initialized

---
+++ lab2/src/Cell.ts
@@ -0,0 +1,19 @@
+class Cell {
+  //   cellValue: number;
+  htmlElement: HTMLElement;
+  constructor(cell: HTMLElement) {
+    this.htmlElement = cell;
+  }
+
+  setCellValue(value: number) {
+    // TODO: napisz samemu metodę, która ustawia w
+    // tabeli kółko i krzyżyk. Możesz przyjąć, że kółko
+    // to wartość -1, krzyżyk to wartość 1, a 0 (zero)
+    // oznacza pole nieustawione.
+    this.htmlElement.addEventListener("click", () => {
+      console.log("klik");
+    });
+  }
+}
+
+export default Cell;
---
+++ lab2/src/app.ts
@@ -0,0 +1,17 @@
+import Animal from "./Animal";
+import Horse from "./Horse";
+import Snake from "./Snake";
+import Cell from "./Cell";
+import Board from "./Board";
+
+// let sam = new Snake("Sammy the Python");
+// let tom: Animal = new Horse("Tommy the Palomino");
+// let tom2: Animal = new Horse("12345");
+// sam.move();
+// tom.move(34);
+
+const table = document.querySelector(".board");
+
+function App(): any {}
+
+App();
