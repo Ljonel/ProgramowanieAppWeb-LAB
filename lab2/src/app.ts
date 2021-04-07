85b4253d5f9654b3373ec3fc8dbc64a82281fcb2
Damian Cygan <damian.cygan@microsoft.wsei.edu.pl>
2021-04-07 23:39:36 +0200

    tictactoe primitive

--- lab2/src/app.ts
+++ lab2/src/app.ts
@@ -1,17 +1,13 @@
-import Animal from "./Animal";
-import Horse from "./Horse";
-import Snake from "./Snake";
-import Cell from "./Cell";
+import Animal from "./Animals/Animal";
+// import Horse from "./Horse";
+// import Snake from "./Snake";
 import Board from "./Board";
-
-// let sam = new Snake("Sammy the Python");
-// let tom: Animal = new Horse("Tommy the Palomino");
-// let tom2: Animal = new Horse("12345");
-// sam.move();
-// tom.move(34);
 
 const table = document.querySelector(".board");
 
-function App(): any {}
+function App(): any {
+  const game = new Board(3);
+  game.init;
+}
 
 App();
